3408d80774ba0db9c2cc602afa2456a6daa3c755
Nachiiet More <nachiketmore.more@gmail.com>
2026-03-24 01:22:25 +0530

    updated auth provider to fix harcoded values

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Auth0Provider
-        domain="dev-7mpty06oczftshr2.us.auth0.com"
-        clientId="yAfTGPuAVz3jPLyPislzQJKeSRVwo7hU"
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-      >
+      domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <App />
     </Auth0Provider>
-  </React.StrictMode>
-  
-
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
